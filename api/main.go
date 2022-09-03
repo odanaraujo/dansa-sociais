1e5bb24817aa984c0f24ca5d76f345075496a8ae
= <=>
2022-09-02 22:39:52 -0300

    criando as rotas, servidor e controllers

--- api/main.go
+++ api/main.go
@@ -12,6 +12,6 @@ func main() {
 
 	r := router.Gerar()
 
-	fmt.Println("Servidor iniciado com sucesso na porta 50")
+	fmt.Println("Servidor iniciado com sucesso na porta 50 ")
 	log.Fatal(http.ListenAndServe(":5000", r))
 }
